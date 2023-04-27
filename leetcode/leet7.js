6a23fe67c9c0aa4116abeb0b76ed602eb0d219d6
PRANAV <pranavprasanth907@gmail.com>
2023-04-27 10:20:15 +0530

    Contains Duplicate

---
+++ leetcode/leet7.js
@@ -0,0 +1,8 @@
+const containsDuplicate = (nums) => {
+    const value = new Set(nums);
+    if(value.size === nums.length) {
+        return false
+    } 
+        return true
+    }
+    console.log(containsDuplicate([4,5,2,1,0,6,0,9,9,8]))
